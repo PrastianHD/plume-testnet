25b05502ccf1208b84c9b4013c06f4eb95695e26
PrastianHD <prastianhidayat08@gmail.com>
2024-07-22 10:23:21 +0700

    Update name

--- utils/name.js
+++ utils/name.js
@@ -1,13 +1,19 @@
 // name.js
-const chalk = require('chalk');
+const chalk = require('chalk')
 
 function printName() {
     console.log(chalk.blueBright(`
-    +==============================================================+                                 
-    |                  ${chalk.yellowBright('Bot All In One Plume Testnet')}                |
-    |                   ${chalk.magentaBright('Author: Prastian Hidayat')}                   |
-    +==============================================================+
+    +=================================================+                                 
+    =           ${chalk.yellowBright('------- PLUME TESTNET -------')}         =
+    =   ${chalk.greenBright('Feature: ')}                                     =    
+    =   ${chalk.greenBright('Menu[1] CheckIn Daily')}                         =
+    =   ${chalk.greenBright('Menu[2] Claim Faucet ETH Every 10 Minutes')}     =
+    =   ${chalk.greenBright('Menu[3] Claim Faucet GOON Every 2 Hours')}       =
+    =   ${chalk.greenBright('Menu[4] Swap GOON/goonUSD ')}                    =
+    =   ${chalk.greenBright('Menu[5] Stake goonUSD in Nest Staking ')}        =
+    =   ${chalk.magentaBright('Author: Prastian Hidayat')}                      =
+    =   ${chalk.magentaBright('Github: https://github.com/PrastianHD')}         =
+    +=================================================+
     `));
 }
-
 module.exports = { printName };
